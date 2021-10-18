ce12d64df845c30d0671158b8ebc6f07a6dd4948
Md-Abdur-Raheem <md.raheem2450@gmail.com>
2021-10-18 13:19:08 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Route>
+          <Header></Header>
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Col, Container, Nav, Navbar ,Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import logo from '../../../media/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header className="navigation-container w-100 sticky-top">
+            <Container>
+                <Row>
+                    <Col md = {4}>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            width="130"
+                            height="50"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                        </Navbar.Brand>
+                    </Col>
+                    <Col md={8}>
+                        <Nav className = "fs-5">
+                            <NavLink className = "p-3 nav-menu" to="#home">Home</NavLink>
+                            <NavLink className = "p-3 nav-menu" to="#link">About</NavLink>
+                            <NavLink className = "p-3 nav-menu" to="#home">Services</NavLink>
+                            <NavLink className = "p-3 nav-menu" to="#link">Doctors</NavLink>
+                            <NavLink className = "p-3 nav-menu" to="#home">Contacts</NavLink>
+                            <NavLink className = "p-3 nav-menu" to="#link">Login</NavLink>
+                        </Nav>
+                    </Col>
+           </Row>
+           </Container>
+        </header>
+    );
+};
+
+export default Header;
